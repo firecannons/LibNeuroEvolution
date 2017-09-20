765be27e4f99f4858d231b303122632adb0e1771
Lucas <lawf48@mst.edu>
2017-09-19 21:42:29 -0500

    Added some comments to Population.h.

--- Source/Population.h
+++ Source/Population.h
@@ -1,3 +1,11 @@
+/*
+    Programmer: Lucas Winkelman
+    File: Population.h
+    Date: 9/19/2017
+    Purpose: Population.h contains definitions for the Population class
+        in LibNeuroEvolution.
+*/
+
 #ifndef POPULATION_H
 #define POPULATION_H
 
@@ -7,20 +15,52 @@ using namespace std ;
 
 namespace LNE
 {
+        // Coefficient of the worst HyperParameterGroups to kill each generation
+        //     they are regenerated.
     const float POPULATION_KILL_AMOUNT = 0.1 ;
+		
+        // Coefficient of the worst HyperParameterGroups to mutate each generation
+        //     they are regenerated.
     const float POPULATION_MUTATE_AMOUNT = 0.1 ;
 
 
     class Population
     {
         public:
-            Population( vector < unsigned int > InSizes , unsigned int InNumberGroups ,
-                       unsigned int InNumberNetworksPerGroup , unsigned int InGroupEvolutionGenerations ) ;
-
+            // Desc: The Population constructor creates a Population based
+            //     on given parameters.
+            // Pre: InSizes must be a vector with length of the number of
+            //     layers you want in your network.  Each element in position n
+            //     must be the number of neurons you want in the nth layer.
+            //     InNumberGroups is the number of HyperParameterGroups in
+            //     the Population.
+            //     InNumberNetworksPerGroup is the number of networks per
+            //     HyperParameterGroup.
+            //     InGroupEvolutionGenerations is the number of generations
+            //     per evolution of the HyperParameterGroups.
+            // Post: A Population class based on the given parameters is
+            //     constructed.
+            Population( vector < unsigned int > InSizes ,
+                unsigned int InNumberGroups ,
+                unsigned int InNumberNetworksPerGroup ,
+                unsigned int InGroupEvolutionGenerations ) ;
+            
+            // Desc: The Population destructor destroys a Population
+            // Pre: The Population must exist.
+            // Post: The Population is destroyed.
             ~ Population ( ) ;
-
+            
+            // Desc: The Population copy constructor constructs a Population
+            //     based on another Population.
+            // Pre: The source Population must exist.
+            // Post: The a new Population is constructed.
             Population( const Population & SourcePopulation ) ;
-
+            
+            // Desc: The = operator will make the destination Population
+            //     equal to the source operator.
+            // Pre: The source population must exist.
+            // Post: The destination Population will have be equal
+            //     to the source Population.
             Population & operator = ( const Population & SourcePopulation ) ;
 
             unsigned int GetCurrentGeneration ( ) const ;
